3436f9a38c586ebccd891c89e9a8b7eb6f62aff9
arijit sahoo <tijira1999sahoo@gmail.com>
2020-04-30 16:31:52 +0530

    TASK1_SOLUTION

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spectrum_ML
+Internship project based on data science concepts
+
+Using Numpy and Matplotlib
---
+++ Scores_dict.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+scores = {"Day 1": 100, "Day 2": 108, "Day 3":112, "Day 4":115, "Day 5":150,
+          "Day 6":178, "Day 7": 143, "Day 8": 132, "Day 9":190, "Day 10": 235,
+          "Day 11":253, "Day 12": 298, "Day 13": 328, "Day 14":390, "Day 15": 257,
+          "Day 16":288, "Day 17": 393, "Day 18": 425, "Day 19":458, "Day 20": 450,
+          "Day 21":473, "Day 22": 333, "Day 23": 452, "Day 24":490, "Day 25": 495,
+          "Day 26":488, "Day 27": 543, "Day 28": 532, "Day 29":590, "Day 30": 605}
